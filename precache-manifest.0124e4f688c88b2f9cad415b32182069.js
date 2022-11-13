7e6cf05d2e011c3816467ef7a219396311ba34fb
yu1ia <vasilenyuky@gmail.com>
2022-11-13 16:09:19 +0200

    Updates

---
+++ precache-manifest.0124e4f688c88b2f9cad415b32182069.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f15fff4bfcbc1101fe11eea89ea6c2c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "2ca8d8baa37e7e875444",
+    "url": "/todo/static/css/main.02cffa6d.chunk.css"
+  },
+  {
+    "revision": "1ad15050d08093cf638f",
+    "url": "/todo/static/js/2.037bb321.chunk.js"
+  },
+  {
+    "revision": "2ca8d8baa37e7e875444",
+    "url": "/todo/static/js/main.54cbc9ab.chunk.js"
+  },
+  {
+    "revision": "6d8c58b621c4051c489a",
+    "url": "/todo/static/js/runtime~main.04b277d6.js"
+  }
+]);
